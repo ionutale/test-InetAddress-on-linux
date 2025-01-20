1a8162080aa87e401a5a46fbdc7eb6ea2ea00642
Ion Utale <ion.utale@guccidigital.com>
2025-01-20 17:58:37 +0100

    i'm batman

---
+++ src/main/java/com/example/test_InetAddress_on_linux/TestInetAddressOnLinuxApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_InetAddress_on_linux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestInetAddressOnLinuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestInetAddressOnLinuxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_InetAddress_on_linux/TestInetAddressOnLinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_InetAddress_on_linux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestInetAddressOnLinuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
